bc27f7b87bcedd3565937f982d84d9ce7b981ad4
peerduha <peerdyxa@gmail.com>
2026-03-05 17:27:42 +0500

    added -y in bootstrap

--- server-bootstrap/bootstrap.sh
+++ server-bootstrap/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo install tree -y
 sudo sed 's/# Port 22/Port 33543' /etc/ssh/sshd_config
 sudo nohup bash ../bootstrap/docker.sh -y
