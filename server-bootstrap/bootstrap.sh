6d16afe91dddc65f7363cae2d639d9acaa0af72b
peerduha <peerdyxa@gmail.com>
2026-03-05 17:20:48 +0500

    changed main script which also will trigger docker.sh

--- server-bootstrap/bootstrap.sh
+++ server-bootstrap/bootstrap.sh
@@ -3,5 +3,5 @@
 sudo apt update && sudo apt upgrade
 sudo install tree -y
 sudo sed 's/# Port 22/Port 33543' /etc/ssh/sshd_config
-sudo bash ../docker.sh -y
+sudo nohup bash ../bootstrap/docker.sh -y
 
