aeb12df942a0841c3872d1cb586e92b05c1472c6
peerduha <peerdyxa@gmail.com>
2026-03-05 17:38:06 +0500

    changed sed command, now should work correctly

--- server-bootstrap/bootstrap.sh
+++ server-bootstrap/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y
-sudo install tree -y
-sudo sed 's/# Port 22/Port 33543' /etc/ssh/sshd_config
+sudo apt install tree -y
+sudo sed 's/# Port 22/Port 33543/' /etc/ssh/sshd_config
 sudo nohup bash ../bootstrap/docker.sh -y
 
